519611ba5c10ae85a4cc119d11d467f16e72ffdd
Backuptick <backuptick@gmail.com>
2022-10-07 14:51:14 +0200

    trial stage

--- poging 1.py
+++ poging 1.py
@@ -3,8 +3,6 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from random import randint, random, choice, gauss
 
-
-
 def geneticAlgorithm(generatieSize, aantalIteraties, startHyperparameters):
     generatie = [changeFunction(startHyperparameters) for _ in range(generatieSize)]
     for _ in range(aantalIteraties):
@@ -12,8 +10,8 @@ def geneticAlgorithm(generatieSize, aantalIteraties, startHyperparameters):
         scores.sort(key = (lambda x: x[0]))                                                         #sorteert het linkerdeel van de tuple en zet deze in een volgorde
         nieuweGeneratie = []
         for _ in range(generatieSize):
-            daddy = scores[random.randint(0, random.randint(0, generatieSize-1))][1]                #pakt rechterdeel van een random tuple in scores (de hyperparameters)
-            mommy = scores[random.randint(0, random.randint(0, generatieSize-1))][1]
+            daddy = scores[randint(0, randint(0, generatieSize-1))][1]                #pakt rechterdeel van een random tuple in scores (de hyperparameters)
+            mommy = scores[randint(0, randint(0, generatieSize-1))][1]
             timmie = crossOver(daddy, mommy)
             timmie = changeFunction(timmie)
             nieuweGeneratie.append(timmie)
@@ -56,31 +54,33 @@ def runNeuralNetwork(hyperParameters: dict):
 
 def changeFunction(hyperParameters: dict):
     output = hyperParameters.copy()
-    parameterToChange = randint(1, 6)                                                               #past aantal parameters aan
+    parameterToChange = randint(1, 6)                                                                               #past aantal parameters aan
     if parameterToChange == 1:
-        if len(output["hidden_layer_sizes"]) < 5 and randint(1, 2) == 1:                            #past aantal layers aan
-            output["hidden_layer_sizes"] = hyperParameters["hidden_layer_sizes"] + randint(3, 70)   #voegt layers toe met een range van een grootte van nodes
-        else:                                                                                       #past aantal nodes in een layer aan
-            layer = randint(0, len(output["hidden_layer_sizes"])-1)
-            nodes = output["hidden_layer_sizes"][layer]
+        if len(output["hidden_layer_sizes"]) < 5 and randint(1, 2) == 1:                                            #past aantal layers aan
+            output["hidden_layer_sizes"] = tuple(list(hyperParameters["hidden_layer_sizes"]) + [randint(3, 70)])           #voegt layers toe met een range van een grootte van nodes
+        else:                                                                                                       #past aantal nodes in een layer aan
+            layerIndex = randint(0, len(output["hidden_layer_sizes"])-1)
+            nodes = output["hidden_layer_sizes"][layerIndex]
             if randint(1, 2) == 1 and nodes > 3:
                 nodes -= min(randint(1, 5), nodes - 3)
             elif nodes < 70: 
                 nodes += min(randint(1, 5), 70 - nodes)
             else: 
                 nodes -= min(randint(1, 5), nodes - 3)
-            output["hidden_layer_sizes"][layer] = nodes
+            layer = list(output["hidden_layer_sizes"])
+            layer[layerIndex] = nodes
+            output["hidden_layer_sizes"] = tuple(layer) 
 
     elif parameterToChange == 2:
         activationFunctions = ["identity", "logistic", "tanh", "relu"]
         activationFunctions.remove(output["activation"])
-        output["activation"] = random.choice(activationFunctions) 
+        output["activation"] = choice(activationFunctions) 
 
     elif parameterToChange == 3:
-        output["alpha"] += gauss(0, 0.001)
+        output["alpha"] = max(gauss(0, 0.001) + output["alpha"], 0.00001)
 
     elif parameterToChange == 4:
-        output["learning_rate_init"] += gauss(0, 0.01)
+        output["learning_rate_init"] = max(gauss(0, 0.01) + output["learning_rate_init"], 0.0001)
 
     elif parameterToChange == 5:
         if output["max_iter"] < 100:
@@ -92,11 +92,17 @@ def changeFunction(hyperParameters: dict):
 
     return output
 
-hyperParameters = {
+startHyperParameters = {
     "hidden_layer_sizes": (100, ),
     "activation": "relu",
     "alpha": 0.0001,
     "learning_rate_init": 0.001,
     "max_iter": 200
 }
-hyperParameters.max_iter
+size = 100
+iterations = 100
+
+result = geneticAlgorithm(size, iterations, startHyperParameters)
+print("score:", result[0], "hyperParameters:", result [1] )
+
+
