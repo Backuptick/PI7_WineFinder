1f91b254d5cf7a9bf36d15d95a098897d3b22998
Backuptick <backuptick@gmail.com>
2022-10-07 23:34:08 +0200

    Finished!

--- poging 1.py
+++ poging 1.py
@@ -1,26 +1,39 @@
+import pandas
 from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from random import randint, random, choice, gauss
 
+def warn(*args, **kwargs):                                                                                         #haalt de warnings weg tijdens het runnen van het programma die door sklearn library erin worden gezet
+    pass
+import warnings
+warnings.warn = warn
+
+data = pandas.read_csv("wine.data", header = None)                                                                 #laadt data in het bestand en print die vervolgens in de terminal
+print(data.iloc[:, 1:])                                                                                            #haalt eerste kolom weg (de 1-3, omdat dat de output is)
+X = data.iloc[:, 1:].values.tolist()
+y = data.iloc[:, 0].values.tolist()
+
 def geneticAlgorithm(generatieSize, aantalIteraties, startHyperparameters):
-    generatie = [changeFunction(startHyperparameters) for _ in range(generatieSize)]
-    for _ in range(aantalIteraties):
+    generatie = [changeFunction(startHyperparameters) for _ in range(generatieSize)]                                #Maakt eerste generatie aan voor alleen eerste keer.
+    for i in range(aantalIteraties):
         scores = [(runNeuralNetwork(x), x) for x in generatie]
-        scores.sort(key = (lambda x: x[0]))                                                         #sorteert het linkerdeel van de tuple en zet deze in een volgorde
+        scores.sort(key = (lambda x: x[0]), reverse = True)                                                         #sorteert het linkerdeel van de tuple en zet deze in een volgorde
         nieuweGeneratie = []
         for _ in range(generatieSize):
-            daddy = scores[randint(0, randint(0, generatieSize-1))][1]                #pakt rechterdeel van een random tuple in scores (de hyperparameters)
+            daddy = scores[randint(0, randint(0, generatieSize-1))][1]                                              #pakt rechterdeel van een random tuple in scores (de hyperparameters)
             mommy = scores[randint(0, randint(0, generatieSize-1))][1]
             timmie = crossOver(daddy, mommy)
             timmie = changeFunction(timmie)
             nieuweGeneratie.append(timmie)
         generatie = nieuweGeneratie
+        print("Iteratie", i, "/", aantalIteraties, "score:", scores[0][0], "/", "average:", sum([x[0] for x in scores]) / len(scores))
     scores = [(runNeuralNetwork(x), x) for x in generatie]
-    scores.sort(key = (lambda x: x[0]))
+    scores.sort(key = (lambda x: x[0]), reverse = True)
+    print("Iteratie", aantalIteraties, "/", aantalIteraties, "score:", scores[0][0], "/", "average:", sum([x[0] for x in scores]) / len(scores))
     return scores[0]
 
-def crossOver(daddy: dict, mommy: dict):
+def crossOver(daddy: dict, mommy: dict):                                                                            #voor iedere key in daddy genereer een random getal (0 of 1) als het 1 is, geef je die aan timmie en als het 0 is geef je die van mommy aan timmie 
     timmie = {}
     for key in daddy.keys():
         if randint(0, 1) == 1:
@@ -37,7 +50,6 @@ def crossOver(daddy: dict, mommy: dict):
 #Maakt functie voor het runnen van het neurale netwerk volgens code site
 
 def runNeuralNetwork(hyperParameters: dict):
-    X, y = make_classification(n_samples=100, random_state=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=1)
     clf = MLPClassifier(
         hidden_layer_sizes = hyperParameters["hidden_layer_sizes"], 
@@ -46,8 +58,6 @@ def runNeuralNetwork(hyperParameters: dict):
         learning_rate_init = hyperParameters["learning_rate_init"],
         max_iter = hyperParameters["max_iter"]
         ).fit(X_train, y_train)
-    clf.predict_proba(X_test[:1])
-    clf.predict(X_test[:5, :])
     return clf.score(X_test, y_test)
 
 #Maakt functie voor verandering van de hyper-parameters.
@@ -100,7 +110,7 @@ startHyperParameters = {
     "max_iter": 200
 }
 size = 100
-iterations = 100
+iterations = 20
 
 result = geneticAlgorithm(size, iterations, startHyperParameters)
 print("score:", result[0], "hyperParameters:", result [1] )
